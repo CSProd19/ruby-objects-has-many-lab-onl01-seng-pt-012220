f8f6ac3f2aa11e2f1ba5e8f908b638736fab82a5
Maria Connie Sanchez <msnchz04@gmail.com>
2020-03-12 06:05:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -23,13 +23,16 @@
 class Post
   attr_accessor :title, :author
   @@all = []
+ 
   def initialize(title)
     @title = title
     @@all << self
   end
+ 
   def self.all
     @@all
   end
+ 
   def author_name
     author.name if author
   end
